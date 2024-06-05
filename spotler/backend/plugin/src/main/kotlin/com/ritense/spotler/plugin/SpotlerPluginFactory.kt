73c17267915f7badeebf0ee5d4e71f7c753a8d65
Floris Thijssen <floris.thijssen@ritense.com>
2024-06-05 13:45:54 +0200

    tp-114320 setup plugin testing

---
+++ spotler/backend/plugin/src/main/kotlin/com/ritense/spotler/plugin/SpotlerPluginFactory.kt
@@ -0,0 +1,14 @@
+package com.ritense.spotler.plugin
+
+import com.ritense.plugin.PluginFactory
+import com.ritense.plugin.service.PluginService
+
+class SpotlerPluginFactory(
+    pluginService: PluginService
+) : PluginFactory<SpotlerPlugin>(pluginService) {
+
+    override fun create(): SpotlerPlugin {
+        return SpotlerPlugin()
+    }
+
+}
