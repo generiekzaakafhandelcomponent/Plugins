e28bd453151cc0ca392f7cf15603f9da8cf336c9
Anna Vissart <anna.vissart@ritense.com>
2024-11-21 11:42:17 +0100

    * configure app with new test case

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,6 +78,7 @@ subprojects {
         if (Os.isFamily(FAMILY_MAC)) {
             println("Configure docker compose for macOs")
             dockerCompose {
+                setProjectName("${rootProject.name}-${project.name}")
                 executable = "/usr/local/bin/docker-compose"
                 dockerExecutable = "/usr/local/bin/docker"
             }
