174e6d7ec8d34e8ca058dfac5cc7fc1b887af957
Boaz van der Wansem <boaz.van.der.wansem@hva.nl>
2025-10-27 13:18:59 +0100

    Fix migration issue

--- frontend/projects/valtimo-plugins/notify-nl/src/public_api.ts
+++ frontend/projects/valtimo-plugins/notify-nl/src/public_api.ts
@@ -23,4 +23,8 @@ export * from './lib/notify-nl-plugin-module';
 export * from './lib/notify-nl-plugin.specification';
 export * from './lib/components/send-sms/send-sms-configuration.component';
 export * from './lib/components/send-email/send-email-configuration.component';
+export * from './lib/components/get-message/get-message-configuration.component';
+export * from './lib/components/get-template/get-template-configuration.component';
+export * from './lib/components/send-letter/send-letter-configuration.component';
+export * from './lib/components/get-all-templates/get-all-templates-configuration.component';
 export * from './lib/components/notify-nl-configuration/notify-nl-configuration.component';
