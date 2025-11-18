fe7f310082838f8413d1e877ae8b4abce4521a54
haiko van der schaaf <hvdschaaf@gmail.com>
2025-11-18 10:22:05 +0100

    fix review issues

--- backend/suwinet-auth/src/main/kotlin/com/ritense/valtimoplugins/suwinetauth/plugin/HttpHeaderInterceptor.kt
+++ backend/suwinet-auth/src/main/kotlin/com/ritense/valtimoplugins/suwinetauth/plugin/HttpHeaderInterceptor.kt
@@ -9,12 +9,8 @@ class HttpHeaderInterceptor(
     val headerValue: String,
     val messagePhase: String = Phase.POST_LOGICAL) : AbstractPhaseInterceptor<Message>(messagePhase) {
 
-
-
     override fun handleMessage(message: Message?) {
         var headers = mapOf<String, List<String>>(headerName to listOf(headerValue))
         message?.put(Message.PROTOCOL_HEADERS, headers)
     }
-
-
 }
