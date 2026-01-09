11ddf9caeb259ccaabf868bb2bb90962c1025b5a
Jasper Kunkeler <jasper@risa-it.nl>
2026-01-09 16:12:20 +0100

    Creating plugin action form

--- frontend/projects/valtimo-plugins/openklant/src/lib/components/open-klant-post-klantcontact/open-klant-post-klantcontact.component.ts
+++ frontend/projects/valtimo-plugins/openklant/src/lib/components/open-klant-post-klantcontact/open-klant-post-klantcontact.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import {FormModule, InputModule} from "@valtimo/components";
+import {PluginTranslatePipeModule} from "@valtimo/plugin";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'lib-open-klant-post-klantcontact',
   standalone: true,
-  imports: [],
+    imports: [
+        FormModule,
+        InputModule,
+        PluginTranslatePipeModule,
+        AsyncPipe
+    ],
   templateUrl: './open-klant-post-klantcontact.component.html',
   styleUrl: './open-klant-post-klantcontact.component.css'
 })
