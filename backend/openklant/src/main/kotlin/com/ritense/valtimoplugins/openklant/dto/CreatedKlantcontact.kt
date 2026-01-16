169280f17f42dc04acf1f5712645f6eb6ac54084
Jasper Kunkeler <jasper@risa-it.nl>
2026-01-16 14:47:15 +0100

    Removing redundant dto

--- backend/openklant/src/main/kotlin/com/ritense/valtimoplugins/openklant/dto/CreatedKlantcontact.kt
+++
@@ -1,7 +0,0 @@
-package com.ritense.valtimoplugins.openklant.dto
-
-data class CreatedKlantcontact(
-    val klantContact: KlantContact,
-    val betrokkene: Betrokkene,
-    val onderwerpobject: Onderwerpobject
-)
