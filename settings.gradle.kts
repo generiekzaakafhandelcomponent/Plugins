77146871514b98add8e565d969034a52e5572662
Axel Plowman <axel@risa-it.nl>
2026-01-22 12:32:11 +0100

    Undo rename

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,7 +11,7 @@ include(
     "backend:mTLS-SSLContext",
     "backend:notify-nl",
     "backend:object-management",
-    "backend:open-klant",
+    "backend:openklant",
     "backend:publictask",
     "backend:rotterdam-oracle-ebs",
     "backend:sample-plugin",
