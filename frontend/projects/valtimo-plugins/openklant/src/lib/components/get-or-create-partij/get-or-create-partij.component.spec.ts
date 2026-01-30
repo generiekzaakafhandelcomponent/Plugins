0cd627d6a0f5fe8d1a936675315d7a6419df7fbd
Jasper Kunkeler <jasper@risa-it.nl>
2026-01-30 16:19:38 +0100

    Fix the test

--- frontend/projects/valtimo-plugins/openklant/src/lib/components/get-or-create-partij/get-or-create-partij.component.spec.ts
+++ frontend/projects/valtimo-plugins/openklant/src/lib/components/get-or-create-partij/get-or-create-partij.component.spec.ts
@@ -1,40 +1,41 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { GetOrCreatePartijComponent } from './get-or-create-partij.component';
+import {GetOrCreatePartijComponent} from './get-or-create-partij.component';
 import {EMPTY, of} from "rxjs";
 import {GetOrCreatePartijConfig} from "../../models/get-or-create-partij-config";
 
 describe('GetOrCreatePartijComponent', () => {
-  let component: GetOrCreatePartijComponent;
-  let fixture: ComponentFixture<GetOrCreatePartijComponent>;
+    let component: GetOrCreatePartijComponent;
+    let fixture: ComponentFixture<GetOrCreatePartijComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [GetOrCreatePartijComponent]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [GetOrCreatePartijComponent]
+        })
+            .overrideTemplate(GetOrCreatePartijComponent, "")
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(GetOrCreatePartijComponent);
-    component = fixture.componentInstance;
+    beforeEach(() => {
+        fixture = TestBed.createComponent(GetOrCreatePartijComponent);
+        component = fixture.componentInstance;
 
-    component.save$ = EMPTY;
-    component.disabled$ = of(false);
-    component.pluginId = "test-plugin";
+        component.save$ = EMPTY;
+        component.disabled$ = of(false);
+        component.pluginId = "test-plugin";
 
-    component.prefillConfiguration$ = of({
-      bsn: "",
-      voorletters: "",
-      voornaam: "",
-      voorvoegselAchternaam: "",
-      achternaam: "",
-    } as GetOrCreatePartijConfig);
+        component.prefillConfiguration$ = of({
+            bsn: "",
+            voorletters: "",
+            voornaam: "",
+            voorvoegselAchternaam: "",
+            achternaam: "",
+        } as GetOrCreatePartijConfig);
 
-    fixture.detectChanges();
-  });
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
