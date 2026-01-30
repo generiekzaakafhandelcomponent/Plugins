59081565b4981b54727b1356c09dbd0d28fd0cf5
Jasper Kunkeler <jasper@risa-it.nl>
2026-01-30 16:01:41 +0100

    Added the content of the getOrCreatePartij component

--- frontend/projects/valtimo-plugins/openklant/src/lib/components/get-or-create-partij/get-or-create-partij.component.spec.ts
+++ frontend/projects/valtimo-plugins/openklant/src/lib/components/get-or-create-partij/get-or-create-partij.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GetOrCreatePartijComponent } from './get-or-create-partij.component';
+import {EMPTY, of} from "rxjs";
+import {GetOrCreatePartijConfig} from "../../models/get-or-create-partij-config";
 
 describe('GetOrCreatePartijComponent', () => {
   let component: GetOrCreatePartijComponent;
@@ -11,9 +13,24 @@ describe('GetOrCreatePartijComponent', () => {
       imports: [GetOrCreatePartijComponent]
     })
     .compileComponents();
-    
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(GetOrCreatePartijComponent);
     component = fixture.componentInstance;
+
+    component.save$ = EMPTY;
+    component.disabled$ = of(false);
+    component.pluginId = "test-plugin";
+
+    component.prefillConfiguration$ = of({
+      bsn: "",
+      voorletters: "",
+      voornaam: "",
+      voorvoegselAchternaam: "",
+      achternaam: "",
+    } as GetOrCreatePartijConfig);
+
     fixture.detectChanges();
   });
 
