81548fadaacba285b35ca235ae734a1e474d43b3
Mika-RisaIT <mika@risa-it.nl>
2026-02-19 13:29:25 +0100

    OpenKlant v13 Sync up/downstream (#299)
    * copy valtimo v12 plugin
    
    * make components not standalone
    
    * change to Valtimo 13 dependencies
    
    * add OpenKlant
    
    * Add version to kotlin-logging
    
    * Change OpenKlantModule to OpenKlantPluginModule. Importing CASE_TAB_TOKEN.
    
    * remove error handling in favor of camunda's build-in handling
    
    * Change logo
    
    * Added openklant to the package.json
    
    * Added tab to public api
    
    * Make app module import consistent with the rest
    
    * Change names of openklant package
    
    * Adding openklant in angular to be consistent of the other plugins
    
    * Onderstaande Melding verholpen
    De velden ['verstrekt_door_partij', 'referentie', 'soort_digitaal_adres'] moeten een unieke set zijn.
    
    * Release 2.0.0 into v13
    
    * Update v13 with the v12 changes
    
    * Version numbering to 2.1.0
    
    * Change openklant case to v13 compatible
    
    * Apply Generic Contact History patch and update file locations
    
    * Add minimal Notify NL environment variables to enable startup
    
    * Add Open Klant environment variables
    
    * Migrate to v13
    
    * Terug draaien env.properties.example Klantinteracties_Url
    
    * Added tabs
    
    * Update test to v13
    
    * Fix tests
    
    * Remove unused @types/jest and @types/mocha
    
    Removed unused type definitions for Jest and Mocha.
    
    * Patch V13
    
    * Patch from v12 to v13
    
    * Undo autoformatting
    
    * Undo autoformatting
    
    * Undo Autoformatting
    
    * Add image for readme
    
    * adjust styling
    
    * Apply Patch v12 -> v13
    
    * Fixing the tab
    
    * Slight v13 readme changes
    
    * Changes to how we trim
    
    * fix color of failed summary text
    
    * Removing dashes out of open-klant
    
    * Add ContactHistoryTabComponent to OpenKlant module
    
    * Move contactgeschiedenis-ophalen to global
    
    * Delete document-upload.process-link.json because there is no matching bpmn
    
    * Change version no in open klant package
    
    * Change frontend properties to v2.2.0
    
    ---------
    
    Co-authored-by: Anish-RisaIT <a.raghoenath@risa-it.nl>
    Co-authored-by: furkang-risa <Furkan@risa-it.nl>
    Co-authored-by: Jasper Kunkeler <jasper@risa-it.nl>
    Co-authored-by: Axel Plowman <axel@risa-it.nl>
    Co-authored-by: JanelTT <177222031+Janel-RisaIT@users.noreply.github.com>

--- backend/openklant/src/main/kotlin/com/ritense/valtimoplugins/openklant/service/OpenKlantService.kt
+++ backend/openklant/src/main/kotlin/com/ritense/valtimoplugins/openklant/service/OpenKlantService.kt
@@ -1,9 +1,12 @@
 package com.ritense.valtimoplugins.openklant.service
 
-import com.ritense.valtimoplugins.openklant.dto.KlantContact
+import com.ritense.valtimoplugins.openklant.dto.Klantcontact
+import com.ritense.valtimoplugins.openklant.dto.Partij
 import com.ritense.valtimoplugins.openklant.model.ContactInformation
-import com.ritense.valtimoplugins.openklant.model.KlantContactOptions
+import com.ritense.valtimoplugins.openklant.model.KlantcontactCreationInformation
+import com.ritense.valtimoplugins.openklant.model.KlantcontactOptions
 import com.ritense.valtimoplugins.openklant.model.OpenKlantProperties
+import com.ritense.valtimoplugins.openklant.model.PartijInformation
 
 interface OpenKlantService {
     suspend fun storeContactInformation(
@@ -11,5 +14,15 @@ interface OpenKlantService {
         contactInformation: ContactInformation,
     ): String
 
-    suspend fun getAllKlantContacten(properties: KlantContactOptions): List<KlantContact>
+    suspend fun getOrCreatePartij(
+        properties: OpenKlantProperties,
+        partijInformation: PartijInformation,
+    ): Partij
+
+    suspend fun getAllKlantcontacten(properties: KlantcontactOptions): List<Klantcontact>
+
+    suspend fun postKlantcontact(
+        properties: OpenKlantProperties,
+        klantcontactCreationInformation: KlantcontactCreationInformation,
+    )
 }
