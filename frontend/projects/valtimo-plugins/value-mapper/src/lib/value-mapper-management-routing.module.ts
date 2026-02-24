5b1d96fd39dc5303c286117cc60676aefda06c43
ayub <ayub.abdulkader@ritense.com>
2026-02-24 16:54:11 +0100

    added the legal/copyright header

--- frontend/projects/valtimo-plugins/value-mapper/src/lib/value-mapper-management-routing.module.ts
+++ frontend/projects/valtimo-plugins/value-mapper/src/lib/value-mapper-management-routing.module.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-2023 Ritense BV, the Netherlands.
+ * Copyright 2015-2026 Ritense BV, the Netherlands.
  *
  * Licensed under EUPL, Version 1.2 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
