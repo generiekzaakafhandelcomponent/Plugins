64533e0386ef53771703210bbe8da079fd8a4207
Jasper Kunkeler <jasper@risa-it.nl>
2026-04-22 11:27:15 +0200

    Added fields in the frontend

--- frontend/projects/valtimo-plugins/openklant/src/lib/models/register-klantcontact-config.ts
+++ frontend/projects/valtimo-plugins/openklant/src/lib/models/register-klantcontact-config.ts
@@ -1,14 +1,17 @@
 export interface RegisterKlantcontactConfig {
-  kanaal: string;
-  onderwerp: string;
-  inhoud: string;
-  vertrouwelijk: string;
-  taal: string;
-  plaatsgevondenOp: string;
-  hasBetrokkene: boolean;
-  partijUuid: string | undefined;
-  voorletters: string | undefined;
-  voornaam: string | undefined;
-  voorvoegselAchternaam: string | undefined;
-  achternaam: string | undefined;
+    referentienummer: string | undefined;
+    kanaal: string;
+    onderwerp: string;
+    inhoud: string | undefined;
+    reactie: string | undefined;
+    vertrouwelijk: string;
+    taal: string;
+    plaatsgevondenOp: string;
+    metadata: string;
+    hasBetrokkene: boolean;
+    partijUuid: string | undefined;
+    voorletters: string | undefined;
+    voornaam: string | undefined;
+    voorvoegselAchternaam: string | undefined;
+    achternaam: string | undefined;
 }
