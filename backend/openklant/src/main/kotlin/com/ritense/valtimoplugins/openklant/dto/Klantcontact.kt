6d48660d4d9a764259afa3ddf2c5e3f001d2a16a
Jasper Kunkeler <jasper@risa-it.nl>
2026-04-23 15:46:19 +0200

    Readded jsonproperties

--- backend/openklant/src/main/kotlin/com/ritense/valtimoplugins/openklant/dto/Klantcontact.kt
+++ backend/openklant/src/main/kotlin/com/ritense/valtimoplugins/openklant/dto/Klantcontact.kt
@@ -1,23 +1,44 @@
 package com.ritense.valtimoplugins.openklant.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class Klantcontact(
+    @field:JsonProperty("uuid")
     override val uuid: String,
+    @field:JsonProperty("url")
     override val url: String,
+    @field:JsonProperty("gingOverOnderwerpobjecten")
     val gingOverOnderwerpobjecten: List<ObjectReference>,
+    @field:JsonProperty("hadBetrokkenActoren")
     val hadBetrokkenActoren: List<Actor>,
+    @field:JsonProperty("omvatteBijlagen")
     val omvatteBijlagen: List<ObjectReference>,
+    @field:JsonProperty("hadBetrokkenen")
     val hadBetrokkenen: List<ObjectReference>,
+    @field:JsonProperty("leiddeTotInterneTaken")
     val leiddeTotInterneTaken: List<ObjectReference>,
+    @field:JsonProperty("nummer")
     val nummer: String?,
+    @field:JsonProperty("referentienummer")
     val referentienummer: String?,
+    @field:JsonProperty("kanaal")
     val kanaal: String,
+    @field:JsonProperty("onderwerp")
     val onderwerp: String,
+    @field:JsonProperty("inhoud")
     val inhoud: String?,
+    @field:JsonProperty("reactie")
     val reactie: String?,
+    @field:JsonProperty("indicatieContactGelukt")
     val indicatieContactGelukt: Boolean?,
+    @field:JsonProperty("taal")
     val taal: String,
+    @field:JsonProperty("vertrouwelijk")
     val vertrouwelijk: Boolean,
+    @field:JsonProperty("plaatsgevondenOp")
     val plaatsgevondenOp: String?,
+    @field:JsonProperty("metadata")
     val metadata: Map<String, String>,
+    @field:JsonProperty("_expand")
     val expand: Any?,
 ) : Referable
--- backend/openklant/src/main/kotlin/com/ritense/valtimoplugins/openklant/dto/KlantcontactCreationRequest.kt
+++ backend/openklant/src/main/kotlin/com/ritense/valtimoplugins/openklant/dto/KlantcontactCreationRequest.kt
@@ -1,6 +1,7 @@
 package com.ritense.valtimoplugins.openklant.dto
 
 import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Pattern
@@ -8,37 +9,48 @@ import jakarta.validation.constraints.Size
 
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 data class KlantcontactCreationRequest(
-    @field:NotBlank
+    @field:JsonProperty("klantcontact")
+    @field:NotNull
     val klantcontact: KlantcontactRequest,
+    @field:JsonProperty("betrokkene")
     val betrokkene: BetrokkeneRequest? = null,
+    @field:JsonProperty("onderwerpobject")
     val onderwerpobject: OnderwerpobjectRequest? = null,
 ) {
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     data class KlantcontactRequest(
+        @field:JsonProperty("referentienummer")
         @field:Size(max = 10)
         val referentienummer: String? = null,
+        @field:JsonProperty("kanaal")
         @field:NotBlank
         @field:Size(max = 50)
         val kanaal: String,
+        @field:JsonProperty("onderwerp")
         @field:NotBlank
         @field:Size(max = 200)
         val onderwerp: String,
+        @field:JsonProperty("inhoud")
         @field:Size(max = 1000)
         val inhoud: String? = null,
+        @field:JsonProperty("reactie")
         @field:Size(max = 1000)
         val reactie: String? = null,
-        @field:NotNull
+        @field:JsonProperty("indicatieContactGelukt")
         val indicatieContactGelukt: Boolean? = null,
+        @field:JsonProperty("taal")
         @field:NotBlank
         @field:Pattern(
             regexp = "^[A-Za-z]{3}$",
             message = "taal must be of 3 letters (ISO 639-2/B)",
         )
         val taal: String,
+        @field:JsonProperty("vertrouwelijk")
         val vertrouwelijk: Boolean,
+        @field:JsonProperty("plaatsgevondenOp")
         @field:NotBlank
         val plaatsgevondenOp: String? = null,
-        @field:NotNull
+        @field:JsonProperty("metadata")
         val metadata: Map<
             @Size(max = 100)
             String,
@@ -49,21 +61,30 @@ data class KlantcontactCreationRequest(
 
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     data class BetrokkeneRequest(
+        @field:JsonProperty("wasPartij")
         val wasPartij: UuidReference? = null,
+        @field:JsonProperty("bezoekadres")
         val bezoekadres: Adres? = null,
+        @field:JsonProperty("correspondentieadres")
         val correspondentieadres: Adres? = null,
+        @field:JsonProperty("contactnaam")
         val contactnaam: Contactnaam? = null,
+        @field:JsonProperty("rol")
         @field:NotBlank
         val rol: Betrokkene.Rol,
+        @field:JsonProperty("organisatienaam")
         @field:Size(max = 200)
         val organisatienaam: String? = null,
+        @field:JsonProperty("initiator")
         @field:NotBlank
         val initiator: Boolean,
     )
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
     data class OnderwerpobjectRequest(
+        @field:JsonProperty("wasKlantcontact")
         val wasKlantcontact: UuidReference? = null,
+        @field:JsonProperty("onderwerpobjectidentificator")
         val onderwerpobjectidentificator: Onderwerpobjectidentificator? = null,
     )
 }
