b8e446412b23dd70e39ca3e7c044055b4920bdf2
Jasper Kunkeler <jasper@risa-it.nl>
2026-04-24 14:16:02 +0200

    Make store contact info standalone

--- frontend/projects/valtimo-plugins/openklant/src/lib/components/open-klant-store-contact-info/open-klant-store-contact-info.component.ts
+++ frontend/projects/valtimo-plugins/openklant/src/lib/components/open-klant-store-contact-info/open-klant-store-contact-info.component.ts
@@ -7,9 +7,9 @@ import {
   Output,
 } from "@angular/core";
 import {
-  FunctionConfigurationComponent,
-  FunctionConfigurationData,
-} from "@valtimo/plugin";
+    FunctionConfigurationComponent,
+    FunctionConfigurationData, PluginTranslatePipeModule,
+} from '@valtimo/plugin';
 import {
   Observable,
   BehaviorSubject,
@@ -18,10 +18,20 @@ import {
   take,
 } from "rxjs";
 import { StoreContactInfoConfig } from "../../models/store-contact-info-config";
+import {FormModule, InputModule} from '@valtimo/components';
+import {AsyncPipe, NgIf} from '@angular/common';
 
 @Component({
-  selector: "store-contact-info",
-  templateUrl: "./open-klant-store-contact-info.component.html"
+    selector: 'store-contact-info',
+    standalone: true,
+    imports: [
+        FormModule,
+        NgIf,
+        InputModule,
+        PluginTranslatePipeModule,
+        AsyncPipe
+    ],
+    templateUrl: './open-klant-store-contact-info.component.html'
 })
 export class StoreContactInfoComponent
   implements FunctionConfigurationComponent, OnInit, OnDestroy {
